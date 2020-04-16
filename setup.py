8cc6a67392de253c9d9e104a29ee587df012aef2
Hans Matos <hans.matos@pucp.edu.pe>
2020-04-16 13:07:46 -0500

    Setup.py has been updated

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import find_packages, setup
 
 setup(
     name='tesis_chatbot',
-    version='1.0.0',
+    author='Hans Matos Rios',
+    author_email='hans.matos@pucp.edu.pe'
+    version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False
