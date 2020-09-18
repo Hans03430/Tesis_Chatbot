f5f18627b6c3da59c32b052d40e2ae6d0c0a8966
Hans Matos <hans.matos@pucp.edu.pe>
2020-09-18 09:35:04 -0500

    EDA done.

--- src/preparation/models/obtained_text.py
+++ src/preparation/models/obtained_text.py
@@ -27,7 +27,7 @@ class ObtainedText(Base):
     grade = Column('GRADE', Integer, nullable=False)
     category = Column('CATEGORY', String, nullable=False)
     cluster_grade = Column('CLUSTER_GRADE', Integer, nullable=True)
-    sentence_pair = relationship('SentencePair', uselist=True, lazy='joined', back_populates='obtained_text', cascade='save-update, delete, delete-orphan, merge')
+    sentence_pair = relationship('SentencePair', uselist=True, lazy='select', back_populates='obtained_text', cascade='save-update, delete, delete-orphan, merge')
     descriptive_index = relationship('DescriptiveIndex', uselist=False, lazy='joined', back_populates='obtained_text', cascade='save-update, delete, delete-orphan, merge')
     connective_index = relationship('ConnectiveIndex', uselist=False, lazy='joined', back_populates='obtained_text', cascade='save-update, delete, delete-orphan, merge')
     lexical_diversity_index = relationship('LexicalDiversityIndex', uselist=False, lazy='joined', back_populates='obtained_text', cascade='save-update, delete, delete-orphan, merge')
