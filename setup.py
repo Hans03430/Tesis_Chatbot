7758579732a2ed0c784d5960fcd7c235d84838ba
Hans Matos <hans.matos@pucp.edu.pe>
2020-11-02 13:53:19 -0500

    ENhanced experiment.

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
         'scikit-learn',
         'scikit-plot',
         'seaborn',
+        'scipy',
         'text-complexity-analyzer-cm @ https://github.com/Hans03430/TextComplexityAnalyzerCM/tarball/master#egg=text-complexity-analyzer-cm-0.1.0'
     ]
 )
